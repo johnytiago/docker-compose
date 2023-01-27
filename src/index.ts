00fdb0c32ba90ac91209d19930091d7887719f20
JT <me@joaotiago.dev>
2023-01-27 00:49:48 +0000

    feat: replace docker-compose with docker compose

--- src/index.ts
+++ src/index.ts
@@ -178,12 +178,16 @@ export const execCompose = (
 
     const cwd = options.cwd
     const env = options.env || undefined
-    const executablePath = options.executablePath || 'docker-compose'
+    const executablePath = options.executablePath || 'docker'
 
-    const childProc = childProcess.spawn(executablePath, composeArgs, {
-      cwd,
-      env
-    })
+    const childProc = childProcess.spawn(
+      executablePath,
+      ['compose', ...composeArgs],
+      {
+        cwd,
+        env
+      }
+    )
 
     childProc.on('error', (err): void => {
       reject(err)
